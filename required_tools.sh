18766e0a826fbfb8693cc0f38dcc3ca061ca6abb
TeK. Solutions & Fabrication <40300295+v1ral-ITS@users.noreply.github.com>
2019-04-19 23:47:01 -0400

    Update required_tools.sh

--- required_tools.sh
+++ required_tools.sh
@@ -3,19 +3,19 @@
 #// operating systems presense of any needed package.
 
 #/#/ Define any tools needed here seperated only by a "\:space:"\
-needed_tool=' yad pastebin cat echo curl xsel '
+needed_tool=' yad pastebin cat echo curl '
 
 #//* Begin System Tool Search
 require_tools ()
 {
     local NOT_AVAIL=""
     for TOOL in $needed_tool; do
-        if [ "`which $TOOL 2> /dev/null`" == "" ];
-then NOT_AVAIL="$NOT_AVAIL $TOOL"; fi
+        if [ "`which $TOOL`" 2> /dev/null == "" ]; then
+	NOT_AVAIL="$NOT_AVAIL $TOOL";
+    fi
     done
     if [[ "$NOT_AVAIL" != "" ]]; then
-        echo "ERROR: The following required tool(s)
-cannot be found: $NOT_AVAIL"
+	echo "ERROR: The following required tool(s) cannot be found: $NOT_AVAIL"
         exit 3
     fi
 }
