8135d2bf0666dcee6987d6ec6269f504c65d5d4a
TeK. Solutions & Fabrication <40300295+v1ral-ITS@users.noreply.github.com>
2019-04-19 22:24:10 -0400

    Create required_tools.sh
    chmod +x this file and run to make sure you have needed files once configuration is done

---
+++ required_tools.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#//+ ITS_ function to easily check the,
+#// operating systems presense of any needed package.
+
+#/#/ Define any tools needed here seperated only by a "\:space:"\
+needed_tool=' yad pastebin cat echo curl '
+
+#//* Begin System Tool Search
+require_tools ()
+{
+    local NOT_AVAIL=""
+    for TOOL in $needed_tool; do
+        if [ "`which $TOOL 2> /dev/null`" == "" ];
+then NOT_AVAIL="$NOT_AVAIL $TOOL"; fi
+    done
+    if [[ "$NOT_AVAIL" != "" ]]; then
+        echo "ERROR: The following required tool(s)
+cannot be found: $NOT_AVAIL"
+        exit 3
+    fi
+}
+
+# Check file system tools before we start
+require_tools || exit 1
